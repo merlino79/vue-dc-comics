f02c00fb226d9c3016522b2fdcf82e5d918b76fb
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-20 22:15:00 +0200

    prova inseromento fonr awesone

---
+++ src/components/FooterBotton.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="cols">
+  <div class="container">
+    <div class="btn">
+      <button>SIGN-UP NOW!</button>
+
+    </div>
+    <div class="follow">
+      <i class="fab fa-500px"></i>
+
+    </div>
+  </div>
+ 
+
+</div>
+  
+</template>
+
+<script>
+export default {
+  name: 'FooterBotton'
+
+}
+</script>
+
+<style lang="scss" scoped>
+.cols{
+  width: 100%;
+  height: 120px;
+  // background-color: #303030;
+}
+
+</style>
