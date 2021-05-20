989b2794891b3c07cc5a4cec42359c02dee29a56
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-20 22:00:35 +0200

    work footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -49,17 +49,117 @@
            </a>
          </li>
        </ul>
-      </div>
-      <div class="dc">
+       <div class="shop">
+         <h3>
+           Shop
+         </h3>
+         <ul>
+           <li>
+             <a href="#">
+               Shop Dc
+             </a>
+           </li>
+           <li>
+             <a href="#">
+               Shop Dc Collection
+             </a>
+           </li>
+         </ul>
+
+        </div>
+        </div>
+        
+        <div class="dc">
         <h3>
           dc
         </h3>
-      </div>
-      <div class="sites">
+        <ul>
+         <li>
+           <a href="#">
+             Terms Of Use
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             Terms Of polici (new)
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             Add Choes
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             Advertisting
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             Subscription
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             Talent Workshopes
+           </a>
+         </li>
+         <li>
+           <a href="#">
+            CPSC Certificates
+           </a>
+         </li>
+          <li>
+           <a href="#">
+            Ratings
+           </a>
+         </li>
+          <li>
+           <a href="#">
+            Shop Helps
+           </a>
+         </li>
+          <li>
+           <a href="#">
+            Contact US
+           </a>
+         </li>
+       </ul>
+         </div>
+        <div class="sites">
         <h3>
-          sites
+          Sites
         </h3>
-      </div>
+        <ul>
+          <li>
+            <a href="#">
+              DC
+            </a>
+            <li>
+            <a href="#">
+              MAD Magazine
+            </a>
+          </li>
+           <li>
+            <a href="#">
+              DC Kings
+            </a>
+          </li>
+           <li>
+            <a href="#">
+             DC Universe
+            </a>
+          </li>
+           <li>
+            <a href="#">
+              DC Power Visa
+            </a>
+          </li>
+          
+          
+        </ul>
+        
+         </div>
 
       </div>
        
@@ -92,28 +192,54 @@ export default {
 .cols{
   background-image: url(../assets/img/footer-bg.jpg);
   width: 100%;
-  height: 350px;
+  height: 370px;
   background-color: #3f443f;
   .link{
     display: flex;
     justify-content: space-between;
-   
-    width: 30%;
+    
+    width: 40%;
     
   }
   ul{
     display: block;
+    
+      
+    
+    
+   
   }
-  a{
-    color: #879587;
+  li{
+     margin-bottom: 5px;
+    a{
+     
+      color: #879587;
+    }
   }
+ 
+
+   
+
+ 
   .dc-comics,
   .dc,
   .sites{
     color: white;
-    margin-top: 50px;
-  }
+   
+    margin-top: 35px;
+    >h3{
+      margin-bottom: 20px;
+    }
+    .shop{
+      margin-top: 20px;
+      >h3{
+        margin-bottom: 20px;
 
+      }
+    }
+     
+  }
+  
   
     
     
